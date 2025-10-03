c7ecbccbba48318ae0b53a4cb456426507a76768
Johan <s1212230@student.windesheim.nl>
2025-10-03 15:12:30 +0200

    test correct aangepast

--- TestCore/TestHelpers.cs
+++ TestCore/TestHelpers.cs
@@ -1,4 +1,5 @@
 using Grocery.Core.Helpers;
+using System.Text;
 
 namespace TestCore
 {
@@ -27,18 +28,29 @@ namespace TestCore
         }
 
 
-        //Unhappy flow
         [Test]
         public void TestPasswordHelperReturnsFalse()
         {
-            Assert.Pass(); //Zelf uitwerken
+            string password = "user1";
+            // geldige base64 salt en een dummy base64 hash van 32 bytes nullen
+            string salt = Convert.ToBase64String(Encoding.UTF8.GetBytes("RandomSalt123456"));
+            string wrongHash = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+            string storedHash = $"{salt}.{wrongHash}";
+
+            Assert.IsFalse(PasswordHelper.VerifyPassword(password, storedHash));
         }
 
-        [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08")]
-        [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA")]
-        public void TestPasswordHelperReturnsFalse(string password, string passwordHash)
+        [TestCase("user1")]
+        [TestCase("user3")]
+        [TestCase("userX")]
+        public void TestPasswordHelperReturnsFalse(string password)
         {
-            Assert.Fail(); //Zelf uitwerken zodat de test slaagt!
+            string salt = Convert.ToBase64String(Encoding.UTF8.GetBytes("RandomSalt123456"));
+            string wrongHash = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+            string storedHash = $"{salt}.{wrongHash}";
+
+            Assert.IsFalse(PasswordHelper.VerifyPassword(password, storedHash));
         }
+
     }
 }
